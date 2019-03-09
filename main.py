bcfc6c16a9168165c97456bc48078540678494b5
Eric Wilson <wilson.eric.n@gmail.com>
2019-03-09 11:13:32 -0500

    change to using classes

--- main.py
+++ main.py
@@ -9,17 +9,29 @@ arcade.set_background_color(arcade.color.LIGHT_KHAKI)
 
 arcade.start_render()
 
-blue_tank = arcade.Sprite("assets/tank_blue.png")
-blue_tank.center_x = 100
-blue_tank.center_y = SCREEN_HEIGHT / 2
-blue_tank.angle = 90
-blue_tank.draw()
 
-green_tank = arcade.Sprite("assets/tank_green.png")
-green_tank.center_x = SCREEN_WIDTH - 100
-green_tank.center_y = SCREEN_HEIGHT / 2
-green_tank.angle = 270
-green_tank.draw()
+class BlueTank(arcade.Sprite):
+
+    def __init__(self):
+        super().__init__("assets/tank_blue.png")
+        self.center_x = 100
+        self.center_y = SCREEN_HEIGHT / 2
+        self.angle = 90
+        self.draw()
+
+
+class GreenTank(arcade.Sprite):
+
+    def __init__(self):
+        super().__init__("assets/tank_green.png")
+        self.center_x = SCREEN_WIDTH - 100
+        self.center_y = SCREEN_HEIGHT / 2
+        self.angle = 270
+        self.draw()
+
+
+blue_tank = BlueTank()
+green_tank = GreenTank()
 
 arcade.finish_render()
 arcade.run()
